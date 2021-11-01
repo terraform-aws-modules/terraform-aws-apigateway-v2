d9263bb43cb29a31f752811d9abbb3edf1427f58
Matt Oddie <github@mattoddie.com>
2021-11-01 14:33:09 +0000

    Update AWS provider min version to 3.24.0

--- examples/complete-http/versions.tf
+++ examples/complete-http/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws    = ">= 2.59"
+    aws    = ">= 3.24.0"
     random = ">= 2.0"
     null   = ">= 2.0"
     tls    = ">= 3.1"
