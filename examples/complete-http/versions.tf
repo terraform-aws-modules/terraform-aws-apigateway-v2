5b54d0558a1ec07eefeddf91b2d87ee2ce24941d
Matt Oddie <github@mattoddie.com>
2021-11-01 20:48:49 +0000

    feat: Add support for response_parameters in integrations (#58)

--- examples/complete-http/versions.tf
+++ examples/complete-http/versions.tf
@@ -2,9 +2,21 @@ terraform {
   required_version = ">= 0.13.1"
 
   required_providers {
-    aws    = ">= 2.59"
-    random = ">= 2.0"
-    null   = ">= 2.0"
-    tls    = ">= 3.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.24.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1"
+    }
   }
 }
