2f8ad61a8668eb7559115f31e62c502ed2ea09c3
Anton Babenko <anton@antonbabenko.com>
2022-06-15 09:37:54 +0200

    feat!: Upgraded AWS provider to v4 everywhere (#74)

--- examples/complete-http/versions.tf
+++ examples/complete-http/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.24"
+      version = ">= 4.0"
     }
     random = {
       source  = "hashicorp/random"
