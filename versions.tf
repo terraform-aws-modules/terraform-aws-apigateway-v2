ef5b4e96ece9c0520f6ed04872741696f05bb787
Bryant Biggs <bryantbiggs@gmail.com>
2025-12-18 12:38:52 -0600

    Apply suggestions from code review

--- versions.tf
+++ versions.tf
@@ -10,7 +10,7 @@ terraform {
 
   provider_meta "aws" {
     user_agent = [
-      "github.com/terraform-aws-modules/terraform-aws-apigateway-v2"
+      "github.com/terraform-aws-modules"
     ]
   }
 }
